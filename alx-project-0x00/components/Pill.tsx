7d86b8ecbcbb74af38e0feee668ba184181bf23d
Emmanuel Godwin <nicensix@gmail.com>
2025-06-08 06:06:55 +0300

    Task 4 completed

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -6,10 +6,10 @@ interface PillProps {
 
 const Pill: React.FC<PillProps> = ({ title }) => {
   return (
-    <div className="bg-gray-200 px-2 py-1 rounded-full text-sm font-medium">
-      {title}
+    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">{title}</p>
     </div>
-  );
-};
+  )
+}
 
 export default Pill;
