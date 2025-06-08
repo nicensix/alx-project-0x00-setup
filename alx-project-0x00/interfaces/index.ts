72074a44a0d98f619e57cd6390cc2f0d967545a3
Emmanuel Godwin <nicensix@gmail.com>
2025-06-08 06:42:50 +0300

    Removed question mark

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -4,7 +4,7 @@ export interface PillProps {
 
 export interface ButtonProps {
   title: string;
-  size?: "small" | "medium" | "large";
-  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
-  styles?: string;
+  size: "small" | "medium" | "large";
+  shape: "rounded-sm" | "rounded-md" | "rounded-full";
+  styles: string;
 }
